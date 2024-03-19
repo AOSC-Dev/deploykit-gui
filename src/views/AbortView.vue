df7ac02eb165927666339a628391d0a356e9dbef
eatradish <sakiiily@aosc.io>
2024-03-19 13:20:05 +0800

    fix: fix `abortView` exiting style

--- src/views/AbortView.vue
+++ src/views/AbortView.vue
@@ -36,24 +36,28 @@ export default {
       {{ $t("abort.p1-1") }}
     </i18n-t>
     <p>{{ $t("abort.p2") }}</p>
+    <DKBottomActions>
+      <DKStripButton
+        omit_bline="1"
+        :text="$t('abort.resume')"
+        @click="$router.back()"
+      >
+        <img src="@/../assets/resume.svg" height="36" />
+      </DKStripButton>
+      <DKStripButton
+        omit_bline="1"
+        :text="$t('abort.save')"
+        @click="quit(false)"
+      >
+        <img src="@/../assets/document-save.svg" height="36" />
+      </DKStripButton>
+      <DKStripButton :text="$t('abort.quit')" @click="quit(true)">
+        <img src="@/../assets/exit-run.svg" height="36" />
+      </DKStripButton>
+    </DKBottomActions>
   </div>
   <div class="loading" v-else>
     <h1>{{ $t("exiting.t1") }}</h1>
     <DKSpinner :title="$t('exiting.l1')" />
   </div>
-  <DKBottomActions>
-    <DKStripButton
-      omit_bline="1"
-      :text="$t('abort.resume')"
-      @click="$router.back()"
-    >
-      <img src="@/../assets/resume.svg" height="36" />
-    </DKStripButton>
-    <DKStripButton omit_bline="1" :text="$t('abort.save')" @click="quit(false)">
-      <img src="@/../assets/document-save.svg" height="36" />
-    </DKStripButton>
-    <DKStripButton :text="$t('abort.quit')" @click="quit(true)">
-      <img src="@/../assets/exit-run.svg" height="36" />
-    </DKStripButton>
-  </DKBottomActions>
 </template>
