f1e21a4253987671b5d97db79095217948feec17
eatradish <sakiiily@aosc.io>
2024-04-28 15:12:26 +0800

    fix: not allow user press exit button on `/abort` view

--- src/App.vue
+++ src/App.vue
@@ -190,7 +190,11 @@ export default {
       @click="on_abort"
       @keyup.enter="on_abort"
       :disabled="!can_quit"
-      v-show="langSelected && isInstall"
+      v-show="
+        langSelected &&
+        isInstall &&
+        $router.currentRoute.value.path !== '/abort'
+      "
     >
       <img
         :alt="$t('d.sr-close-icon')"
