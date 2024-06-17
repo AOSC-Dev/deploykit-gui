c4d8896bc6d362c94ff1ad84e8a5fe23c4f28310
eatradish <sakiiily@aosc.io>
2024-06-18 00:46:29 +0800

    fix: fix execute nmtui ...
    ... 今日有酒金朝醉，但喝酒能不能不要碰代码？

--- src/views/NetworkView.vue
+++ src/views/NetworkView.vue
@@ -45,7 +45,7 @@ export default {
       try {
         this.loading = true;
         this.running = true;
-        await invoke('run_nmtui', { lang: this.config.locale.locale });
+        await invoke('run_nmtui');
         this.loading = false;
         this.running = false;
         this.$router.back();
